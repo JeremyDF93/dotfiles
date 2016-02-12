9bc64db30ae5babd4a526b19646c96821072b8b2
Jeremy Farmer <jeremydf93@hotmail.com>
2016-02-12 07:00:26 -0600

    correct indentation

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,49 +1,49 @@
 y() {
-  yaourt "$@";
+    yaourt "$@";
 }
 
 p() {
-  sudo pacman "$@";
+    sudo pacman "$@";
 }
 
 fn() {
-  local name="$1"
-  shift
-  find . -iname "*$name*" "$@"
+    local name="$1"
+    shift
+    find . -iname "*$name*" "$@"
 }
 
 gr() {
-  grep -r "$@" .
+    grep -r "$@" .
 }
 
 lsvpk() {
-  find . -type f -name "*_dir.vpk" -exec vpk l "{}" \;
+    find . -type f -name "*_dir.vpk" -exec vpk l "{}" \;
 }
 
 luna() {
-  curl -F "f=@$1" -F "l=1" https://luna.punked.us/
+    curl -F "f=@$1" -F "l=1" https://luna.punked.us/
 }
 
 lunas() {
-  curl -F "s=$@" https://luna.punked.us/
+    curl -F "s=$@" https://luna.punked.us/
 }
 
 dos2unix() {
-  sed -i $'s/\r$//' "$@"
+    sed -i $'s/\r$//' "$@"
 }
 
 ssu() {
-  sudo -u ${1:-root} -i
+    sudo -u ${1:-root} -i
 }
 
 man() {
-  env LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-  LESS_TERMCAP_md=$(printf "\e[1;31m") \
-  LESS_TERMCAP_me=$(printf "\e[0m") \
-  LESS_TERMCAP_se=$(printf "\e[0m") \
-  LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-  LESS_TERMCAP_ue=$(printf "\e[0m") \
-  LESS_TERMCAP_us=$(printf "\e[1;32m") \
-  man "$@"
+    env LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
 }
 
