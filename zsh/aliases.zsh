45ee16b99d4c1b7c488b1cf9e9007d9f5e9da0fe
Jeremy Farmer <jeremydf93@hotmail.com>
2017-10-26 02:05:25 -0500

    [luna] scp folders

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,5 @@ alias grep='grep -n --color=auto'
 alias emptyswap='sudo swapoff -a && sudo swapon -a && echo ":: Done"'
 alias xclip='xclip -sel clip'
 alias reload='source ~/.zshrc'
+alias ll='exa -lg'
 
